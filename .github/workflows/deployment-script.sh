7d7e9ada9f5609f7456e6f845c71dffe0fe40991
Jason Freeberg <jafreebe@microsoft.com>
2019-10-18 18:35:26 -0700

    Create deployment-script.sh

---
+++ .github/workflows/deployment-script.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo '👍 INSTALLING THE GEM BUNDLE'
+bundle install
+bundle list | grep "jekyll ("
+echo '👍 BUILDING THE SITE'
+bundle exec jekyll build
+echo '👍 PUSHING IT BACK TO GITHUB-PAGES'
+cd build
+remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
+remote_branch="gh-pages" && \
+git init && \
+git config user.name "${GITHUB_ACTOR}" && \
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
+git add . && \
+echo -n 'Files to Commit:' && ls -l | wc -l && \
+git commit -m'action build' > /dev/null 2>&1 && \
+git push --force $remote_repo master:$remote_branch > /dev/null 2>&1 && \
+rm -fr .git && \
+cd ../
+echo '👍 GREAT SUCCESS!'
