bf27a23a4751d38248a9504540d0f4a76a31e68c
Jason Freeberg <jr.freeberg@comcast.net>
2019-10-19 13:35:34 -0700

    Update deployment directory

--- deployment-script.sh
+++ deployment-script.sh
@@ -5,7 +5,7 @@ bundle list | grep "jekyll ("
 echo '👍 BUILDING THE SITE'
 bundle exec jekyll build
 echo '👍 PUSHING IT BACK TO GITHUB-PAGES'
-cd build
+cd _site
 remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
 remote_branch="gh-pages" && \
 git init && \
@@ -13,7 +13,7 @@ git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
 git add . && \
 echo -n 'Files to Commit:' && ls -l | wc -l && \
-git commit -m'action build' > /dev/null 2>&1 && \
+git commit -m 'Automated build and deploy via Github Action' > /dev/null 2>&1 && \
 git push --force $remote_repo master:$remote_branch > /dev/null 2>&1 && \
 rm -fr .git && \
 cd ../
